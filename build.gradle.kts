3a4b1c7d66812958447b6d6e2f3ca0a33c360b81
Sean Simpson <JediTiger@users.noreply.github.com>
2026-05-06 23:38:45 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.maintenancerecordsapp"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springAiVersion"] = "2.0.0-M5"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.ai:spring-ai-starter-model-openai")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("org.xerial:sqlite-jdbc")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
